bd6058f156f5ecb175ce3479410a0964ca485db0
M3tox <75583358+M3tox@users.noreply.github.com>
2021-08-10 12:35:53 +0200

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,30 @@
+//{{NO_DEPENDENCIES}}
+
+#define IDR_ACCELERATOR1                101
+#define IDR_MENU1                       103
+#define IDD_DIALOG1                     104
+#define IDD_PROPPAGE_LARGE              107
+#define IDC_BUTTON1                     1001
+#define IDC_CHECK1                      1002
+#define IDC_CHECK2                      1003
+#define IDC_CHECK3                      1004
+#define IDC_BUTTON2                     1005
+#define IDC_BUTTON3                     1006
+#define IDC_STATIC1                     1008
+#define IDC_CHECK4                      1009
+#define IDC_EDIT1                       1010
+#define ID_ACCELERATOR40001             40001
+#define ID_FILE_OPENFILE                40003
+#define ID_FILE_CLOSE                   40004
+#define ID_FILE_EXTRACT                 40005
+#define ID_ABOUT                        40006
+
+// Next default values for new objects
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_COMMAND_VALUE         40007
+#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
