990bee7fac80f4b34a306e66af199e1034ce709a
M3tox <75583358+M3tox@users.noreply.github.com>
2022-04-29 14:36:10 +0200

    Update resource.h

--- resource.h
+++ resource.h
@@ -1,4 +1,6 @@
 //{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by DFET.rc
 
 #define IDR_ACCELERATOR1                101
 #define IDR_MENU1                       103
@@ -19,12 +21,13 @@
 #define ID_FILE_EXTRACT                 40005
 #define ID_ABOUT                        40006
 
-// Next default values for new objects
+// Nächste Standardwerte für neue Objekte
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
-#define _APS_NEXT_COMMAND_VALUE         40007
-#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
